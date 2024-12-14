e8e933bf50bc8cf86c9ffe7acc85b8af7444ad9b
Akshansh8 <akshanshsharma2001@gmail.com>
2024-12-15 00:29:05 +0530

    WIP

--- src/Alphabet.java
+++ src/Alphabet.java
@@ -1,4 +1,14 @@
 
 public class Alphabet {
 
+	public static final String UPPERCASE_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	public static final String LOWERCASR_LETTERS = "abcdefghijklmnopqrstuvwxyz";
+	public static final String NUMBERS = "1234567890";
+	public static final String SYMBOLS = "!@#$%^&*()-_=+\\/~?";
+	
+	private final StringBuilder pool;
+	
+	
+
+	
 }
